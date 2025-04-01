a059d236b01bf3b11d32f6e5a8620c0b3e02cc93
ecresp1el <56608164+ecresp1el@users.noreply.github.com>
2025-04-01 11:22:36 -0400

    removed unnecessary test file

--- tests/test_image_analysis.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-from BL_CalciumAnalysis.image_analysis_methods_umich import process_image
-
-def test_process_image():
-    img = np.ones((100, 100), dtype=np.uint8) * 255
-    processed = process_image(img)
-    assert processed.shape == img.shape
